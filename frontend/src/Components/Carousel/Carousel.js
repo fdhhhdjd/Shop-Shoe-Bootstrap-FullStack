767807ec65a66fff804808ca2d3580f2d0ef6270
tai <nguyentientai10@gmail.com>
2022-03-24 22:20:47 +0700

    fix uncheck

--- frontend/src/Components/Carousel/Carousel.js
+++ frontend/src/Components/Carousel/Carousel.js
@@ -1,8 +1,9 @@
 import lottie from "lottie-web";
 import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 const Carousel = ({ handleScrollMenu }) => {
   const Scroll = useRef();
-
+  const { Information } = useSelector((state) => ({ ...state.info }));
   useEffect(() => {
     lottie.loadAnimation({
       container: Scroll.current,
@@ -12,78 +13,70 @@ const Carousel = ({ handleScrollMenu }) => {
       animationData: require("../utils/ScrollToTop.json"),
     });
   }, []);
+  const carousel = Information && Information.carousels;
+
   return (
-    <>
-      <div className="container-fluid ">
-        <div className="row">
-          {/* slider banner	 */}
+    <React.Fragment>
+      {carousel && (
+        <div className="container-fluid">
           <div
-            id="carouselExampleIndicators"
-            className="carousel slide"
+            id="myCarousel"
+            className="carousel slide border"
             data-ride="carousel"
           >
-            <ol className="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to={0}
-                className="active"
-              />
-              <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-              <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-            </ol>
             <div className="carousel-inner">
-              <div className="carousel-item active">
-                <div className="info">
-                  <h1>SLIDE ONE</h1>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="info">
-                  <h1>SLIDE TWO</h1>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="info">
-                  <h1>SLIDE THREE</h1>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  </p>
-                </div>
-              </div>
+              {carousel.map((item) => {
+                return (
+                  <>
+                    <div className="carousel-item active">
+                      <img
+                        className="d-block w-100 "
+                        src={item.image && item.image.url}
+                        alt="Leopard"
+                      />
+                      <div className="carousel-caption d-none d-sm-block">
+                        <h2>{item.heading}</h2>
+                        <small>{item.description}</small>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+              <a className="carousel-control-prev" onClick={handleScrollMenu}>
+                <span className="sr-oy">
+                  <div ref={Scroll}></div> Here
+                </span>
+              </a>
+
+              <a
+                className="carousel-control-prev"
+                href="#myCarousel"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Previous</span>
+              </a>
+              <a
+                className="carousel-control-next"
+                href="#myCarousel"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Next</span>
+              </a>
             </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span className="carousel-control-prev-icon" aria-hidden="true" />
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span className="carousel-control-next-icon" aria-hidden="true" />
-              <span className="sr-only">Next</span>
-            </a>
-            <a className="carousel-control-prev" onClick={handleScrollMenu}>
-              <span className="sr-oy">
-                <div ref={Scroll}></div> Here
-              </span>
-            </a>
           </div>
         </div>
-      </div>
-    </>
+      )}
+    </React.Fragment>
   );
 };
 
