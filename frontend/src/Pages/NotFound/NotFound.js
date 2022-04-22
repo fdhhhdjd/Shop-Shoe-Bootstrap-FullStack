d27b26c858e0a907c992d49a4cf451023c0ac6e3
tai <nguyentientai10@gmail.com>
2022-04-22 22:02:04 +0700

    fix

--- frontend/src/Pages/NotFound/NotFound.js
+++ frontend/src/Pages/NotFound/NotFound.js
@@ -18,7 +18,7 @@ const NotFound = () => {
             className="col-md-3 col-sm-6 col-12 btn btn-success mt-5"
             onClick={() => navigate(-1)}
           >
-            <span className="text-white text-decoration-none">Back Page</span>
+            <span className="text-white text-decoration-none">Back Page.</span>
           </button>
         </div>
       </div>
