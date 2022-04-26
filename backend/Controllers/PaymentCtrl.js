1c4b6b6fdb2668310b2e093f598d42c2f8e6c8a2
tai <nguyentientai10@gmail.com>
2022-04-26 23:18:28 +0700

    fix

--- backend/Controllers/PaymentCtrl.js
+++ backend/Controllers/PaymentCtrl.js
@@ -2,8 +2,8 @@ const Payments = require("../Model/PaymentModel");
 const Users = require("../Model/userModel");
 const bcrypt = require("bcrypt");
 const STORAGE = require("../utils/Storage");
-// const Stripe = require("Stripe");
-// const stripe = Stripe(process.env.STRIPE_KEY);
+const Stripe = require("Stripe");
+const stripe = Stripe(process.env.STRIPE_PUBLIC);
 const paymentCtrl = {
   //Get All Payment
   getPayments: async (req, res) => {
@@ -17,84 +17,84 @@ const paymentCtrl = {
     }
   },
   //Payment Stripe
-  // StripePayments: async (req, res) => {
-  //   const line_items = req.body.cartItems.map((item) => {
-  //     return {
-  //       price_data: {
-  //         currency: "usd",
-  //         product_data: {
-  //           name: item.name,
-  //           images: [item.image.url],
-  //           description: item.description,
-  //           metadata: {
-  //             id: item._id,
-  //           },
-  //         },
-  //         unit_amount: item.price * 100,
-  //       },
-  //       quantity: item.quantity,
-  //     };
-  //   });
-  //   const session = await stripe.checkout.sessions.create({
-  //     payment_method_types: ["card"],
-  //     shipping_address_collection: {
-  //       allowed_countries: ["US", "CA", "KE"],
-  //     },
-  //     shipping_options: [
-  //       {
-  //         shipping_rate_data: {
-  //           type: "fixed_amount",
-  //           fixed_amount: {
-  //             amount: 0,
-  //             currency: "usd",
-  //           },
-  //           display_name: "Free shipping",
-  //           // Delivers between 5-7 business days
-  //           delivery_estimate: {
-  //             minimum: {
-  //               unit: "business_day",
-  //               value: 5,
-  //             },
-  //             maximum: {
-  //               unit: "business_day",
-  //               value: 7,
-  //             },
-  //           },
-  //         },
-  //       },
-  //       {
-  //         shipping_rate_data: {
-  //           type: "fixed_amount",
-  //           fixed_amount: {
-  //             amount: 1500,
-  //             currency: "usd",
-  //           },
-  //           display_name: "Next day air",
-  //           // Delivers in exactly 1 business day
-  //           delivery_estimate: {
-  //             minimum: {
-  //               unit: "business_day",
-  //               value: 1,
-  //             },
-  //             maximum: {
-  //               unit: "business_day",
-  //               value: 1,
-  //             },
-  //           },
-  //         },
-  //       },
-  //     ],
-  //     phone_number_collection: {
-  //       enabled: true,
-  //     },
-  //     customer_email: req.body.email,
-  //     line_items,
-  //     mode: "payment",
-  //     success_url: `${process.env.FRONTEND_URL}/success`,
-  //     cancel_url: `${process.env.FRONTEND_URL}/cart`,
-  //   });
-  //   res.send({ url: session.url });
-  // },
+  StripePayments: async (req, res) => {
+    const line_items = req.body.cartItems.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+            images: [item.image.url],
+            description: item.description,
+            metadata: {
+              id: item._id,
+            },
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      };
+    });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA", "KE"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      phone_number_collection: {
+        enabled: true,
+      },
+      customer_email: req.body.email,
+      line_items,
+      mode: "payment",
+      success_url: `https://shopshoetaiheo.herokuapp.com/success`,
+      cancel_url: `https://shopshoetaiheo.herokuapp.com/cart`,
+    });
+    res.send({ url: session.url });
+  },
   getPaymentDeletes: async (req, res) => {
     try {
       const payments = await Payments.find({ deleteAt: true }).populate(
